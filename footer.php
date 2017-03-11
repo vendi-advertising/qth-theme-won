6f935559416ff2b290c0416cceb6cdd968e63dbc
chris@vendiadvertising.com <chris@vendiadvertising.com>
2017-03-11 14:05:43 -0600

    First commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Qth-theme-won
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'qth-theme-won' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'qth-theme-won' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'qth-theme-won' ), 'qth-theme-won', '<a href="https://automattic.com/" rel="designer">Me</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
